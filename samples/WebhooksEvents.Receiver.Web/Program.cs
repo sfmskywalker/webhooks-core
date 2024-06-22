52ca0b42d52c0d313c3d234bebe23e9d3cd5a32d
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-06-22 22:06:35 +0200

    Update webhook event processing and endpoint
    The webhook processing in the WebhooksEvents Receiver has been updated to include general and specific event types mapping. The '/webhooks' endpoint now processes general webhook events, while the '/webhooks/heartbeat' processes a specified Heartbeat event. In addition, the Url for Heartbeat events in the WebhookEvents Generator has been updated accordingly.

--- samples/WebhooksEvents.Receiver.Web/Program.cs
+++ samples/WebhooksEvents.Receiver.Web/Program.cs
@@ -8,10 +8,15 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapPost("/webhooks", (WebhookEvent<IDictionary<string, object>> webhookEvent) =>
+app.MapPost("/webhooks", (WebhookEvent webhookEvent) =>
 {
-    //var heartbeat = webhookEvent.Payload!;
-    //Console.WriteLine($"Heartbeat event received at: {heartbeat.Timestamp}");
+    Console.WriteLine($"{webhookEvent.EventType} event received at: {webhookEvent.Timestamp}");
+});
+
+app.MapPost("/webhooks/heartbeat", (WebhookEvent<Heartbeat> webhookEvent) =>
+{
+    var heartbeat = webhookEvent.Payload!;
+    Console.WriteLine($"Heartbeat at {heartbeat.Timestamp}");
 });
 
 app.Run();
